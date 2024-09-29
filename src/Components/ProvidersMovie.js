b2b82b823017c19bfb3af6947a712270830c0504
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-09-29 18:22:34 +0400

    carousel is under process, left and right arrow button added in small screen

--- src/Components/ProvidersMovie.js
+++ src/Components/ProvidersMovie.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useProviders from "../CustomHooks/useProviders"
 import { useSelector } from 'react-redux';
-import { POSTER_URL, NO_IMAGE } from '../Constants/apiKey';
+import { POSTER_URL } from '../Constants/apiKey';
 const ProvidersMovie = () => {
 
     useProviders();
     const providers_data = useSelector((store) => store.movie.providers)
     const { results } = providers_data;
     console.log("..", results)
+    const [transValue,setTransValue] = useState(0)
+
     if (providers_data === "") return null
+    const x = results.slice(0,200);
+
     return (
         <div>
-            <div className='w-screen h-[100px] flex overflow-scroll mb-5 bg-blue-500 items-center relative'>
-                <h1 className='text-white text-xl font-bold absolute top-0 left-0'>Providers</h1>
-                <div className='flex transition-transform duration-1000 ease-out  gap-3 px-5 bg-green-400'>
-                    {results.map((pro)=><img src={POSTER_URL+pro.logo_path} className='w-11 h-11 object-contain rounded-xl'/>)}
+            <div className='w-screen h-[75px] flex  mb-5 bg-blue-500 items-center  relative'>
+                    <button 
+                        className='w-10 bg-red-500  top-1/2 left-0 -translate-y-1/2 z-10 absolute'
+                        onClick={
+                            ()=>setTransValue(transValue-1350)
+                        }
+                    >Left</button>
+                    <button className='w-10 bg-red-500 absolute top-1/2 -translate-y-1/2 right-0 z-10'
+                         onClick={
+                            ()=>setTransValue(transValue+500)
+                        }       
+                    >Right</button>
+                    <div className='overflow-scroll bg-green-700 w-screen h-[70%]'>
+                        <div 
+                    className='flex transition-transform duration-1000 ease-out  gap-6 px-10 bg-green-400 '
+                    style={{ transform: `translateX(${transValue}px)`}}
+                >
+                    {x.map((pro)=><img src={POSTER_URL+pro.logo_path}  key = {pro.provider_id} className='w-11 h-11 object-contain rounded-xl'/>)}
                 </div>
+                    </div>
+                
+                
             </div>
         </div>
     )
